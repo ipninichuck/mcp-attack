85caa0f38730b0b177ccbf02469cd508b7465935
Ivan Ninichuck <ipninichuck@gmail.com>
2025-12-02 05:03:12 -0800

    Add configuration constants for attack STIX data

---
+++ src/attack_mcp/config.py
@@ -0,0 +1,5 @@
+import os
+
+ATTACK_STIX_URL = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack-18.1.json"
+DEFAULT_NAVIGATOR_VERSION = "4.8.0"
+DEFAULT_LAYER_VERSION = "4.4"
